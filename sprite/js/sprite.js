43d06657e355786e6b55f3065860601a335cbcd2
maiordom <maiordom@yandex.ru>
2013-05-26 22:19:00 +1100

    upd sprite

--- sprite/js/sprite.js
+++ sprite/js/sprite.js
@@ -6,7 +6,7 @@ Sprite.Model.CanvasElement = Backbone.Model.extend({
         h: 0,
         name: null,
         fileEntity: null,
-        fileURL: null
+        fileContent: null
     },
 
     initialize: function() {
@@ -27,218 +27,41 @@ Sprite.Model.CanvasElement = Backbone.Model.extend({
                 $this.set({
                     w: e.target.width,
                     h: e.target.height,
-                    fileURL: this.src
-                });    
-                $this.trigger( 'onloadFile' );            
+                    fileContent: this.src
+                });
+                $this.trigger( 'onloadFile' );
             };
             img.src = e.target.result;
         }
 
-        reader.readAsDataURL( this.get( 'fileEntity' ) );    
+        reader.readAsDataURL( this.get( 'fileEntity' ) );
     }
 });
 
 Sprite.Collection.CanvasElements = Backbone.Collection.extend({
-    model: Sprite.Model.CanvasElement
-});
-
-Sprite.Collection.CanvasElements = new Sprite.Collection.CanvasElements();
-
-Sprite.View.Document = Backbone.View.extend({
-    events: {
-        'dragover':  'nullfunc',
-        'dragenter': 'nullfunc',
-        'dragexit':  'nullfunc',
-        'drop':      'dropfunc',
-    },
-
-    initialize: function() {
-        this.InnerCanvas = new Sprite.View.CanvasInner();
-        this.OuterCanvas = new Sprite.View.CanvasOuter();
-
-        this.$el       = $( '.canvas-box' );
-        this.cssBox    = $( '.css-view-box' );
-        this.cssScroll = $( '.css-view-scroll' );
-        this.cssInner  = $( '.css-view-inner' );
-        this.cssDesc   = $( '.css-description');
-        this.wrapper   = $( '.wrapper' );
-        this.win       = $( window );
-        this.doc       = $( document );
-        this.body      = $( 'body' );
-        this.sldEl     = $( {} );
-        this.dragObj   = {
-            mouseOffset: {
-                x: null,
-                y: null
-            },
-            elModel: null
-        }
-
-        this.$el.append( this.InnerCanvas.el );
-        this.$el.append( this.OuterCanvas.el );
-        this.InnerCanvas.render();
-        this.OuterCanvas.render();
-
-        this.setCSSScrollParams( 350, this.win.height() - this.cssDesc.height() );
-        this.setCSSScrollPane();
-        this.setElPosition();
-        this.bindEvents();
-    },
-
-    bindEvents: function() {
-        var self = this;
-
-        this.win.on( 'resize', _.bind( this.onWinResize, this ) );
-
-        this.$el.on( 'mousedown', '.canvas-element', function( e ) {
-            self.sldEl.removeClass( 'canvas-element-selected' );
-            self.sldEl = $( this ).addClass( 'canvas-element-selected' );
-            self.dragEngine( self.sldEl, e );
+    model: Sprite.Model.CanvasElement,
+    createSprite: function() {
+        var reqData = [], serializedData = [], tmp;
+        this.each( function( elModel ) {
+            tmp = elModel.toJSON();
+            delete tmp.fileEntity;
+            reqData.push( tmp );
         });
 
-        this.$el.on( 'click', '.canvas-element', function() {
-            self.sldEl.removeClass( 'canvas-element-selected' );
-            self.sldEl = $( this ).toggleClass( 'canvas-element-selected' );
-        });
-
-        this.$el.on( 'mouseenter', '.canvas-element', function() {
-            $( this ).addClass( 'canvas-element-hover' );
-        });
-
-        this.$el.on( 'mouseleave', '.canvas-element', function() {
-            $( this ).removeClass( 'canvas-element-hover' );
-        });
-    },
-
-    setElPosition: function() {
-        var rect = this.$el.offset();
-        this.rect = {
-            x:    rect.left,
-            xmax: rect.left + this.InnerCanvas.options.width,
-            y:    rect.top,
-            ymax: rect.top + this.InnerCanvas.options.height
-        };
-    },
-
-    dragEngine: function( el, e ) {
-        var modelId = el.data( 'id' );
-
-        this.dragObj.elModel = Sprite.Collection.CanvasElements.get( modelId ),
-        this.dragObj.mouseOffset = {
-            x: e.pageX - this.rect.x - this.dragObj.elModel.get( 'x' ),
-            y: e.pageY - this.rect.y - this.dragObj.elModel.get( 'y' )
-        };
-
-        this.onDragStart();
-        this.doc.mousemove( _.bind( this.onDragMove, this ) );
-        this.doc.mouseup( _.bind( this.onDragEnd, this ) );
-    },
-
-    onDragMove: function( e ) {
-        this.dragMove( e, this.dragObj.mouseOffset, this.dragObj.elModel );
-    },
-
-    dragMove: function( e, mouseOffset, elModel ) {
-        var rect = this.rect,
-            x = e.pageX - rect.x - mouseOffset.x,
-            y = e.pageY - rect.y - mouseOffset.y;
-
-        if ( x < 0 ) x = 0;
-        if ( y < 0 ) y = 0;
-        if ( x + elModel.get( 'w' ) > this.InnerCanvas.options.width  ) x = this.InnerCanvas.options.width  - elModel.get( 'w' );
-        if ( y + elModel.get( 'h' ) > this.InnerCanvas.options.height ) y = this.InnerCanvas.options.height - elModel.get( 'h' );
-
-        elModel.set( 'x', x );
-        elModel.set( 'y', y );
-        elModel.trigger( 'dragmove' );
-
-        e.preventDefault();
-        e.stopPropagation();
-    },
-
-    onDragStart: function() {
-        this.body[ 0 ].style.cursor = 'move';
-        this.body.addClass( 'drag' );
-        document.ondragstart = function() { return false };
-        document.body.onselectstart = function() { return false };
-    },
-
-    onDragEnd: function() {
-        this.doc.off( 'mouseup mousemove' );
-        this.body.css( 'cursor', 'auto' ).removeClass( 'drag' );
-        document.ondragstart = null;
-        document.body.onselectstart = null;
-    },
-
-    onWinResize: function() {
-        this.setCSSScrollParams( 350, this.win.height() - this.cssDesc.height() );
-        this.setCSSScrollPane();
-    },
-
-    setCSSScrollParams: function( w, h ) {
-        this.cssScroll.css({
-            width: w,
-            height: h
-        });        
-    },
-
-    setCSSScrollPane: function() {
-        this.cssScroll.jScrollPane({
-            verticalGutter: 0
-        });
-    },
-
-    createElement: function( modelParams ) {
-        var self = this,
-            canvasElModel = new Sprite.Model.CanvasElement( modelParams ),
-            canvasElView  = new Sprite.View.CanvasElement( { model: canvasElModel } ),
-            cssElView     = new Sprite.View.CSSElement( { model: canvasElModel } );
-
-        Sprite.Collection.CanvasElements.add( canvasElModel );
-
-        canvasElModel.on( 'onloadFile', function() {
-            canvasElView.render();
-            cssElView.render();
-
-            self.$el.append( canvasElView.el );
-            self.cssInner.append( cssElView.el );
-            self.setCSSScrollPane();
-        });
-    },
-
-    nullfunc: function( e ) {
-        e.preventDefault();
-        e.stopPropagation();
-    },
-
-    dropfunc: function( e ) {
-        var files = e.originalEvent.dataTransfer.files,
-            len = files.length,
-            file,
-            $target = $( e.target ),
-            xPos = Math.floor( e.originalEvent.clientX - $target.offset().left ),
-            yPos = Math.floor( e.originalEvent.clientY - $target.offset().top ),
-            classname;
-
-        this.nullfunc( e );
-
-        for ( var i = 0; i < len; i++ ) {
-            file = files[ i ];
-            classname = file.name.split( '.' );
-            classname.pop();
-            classname = classname.join( '.' ).replace( / /, '-' );
-
-            if ( classname.search( /^\d/ ) !== -1 ){
-                classname = 's-' + classname;
-            }
-
-            this.createElement({
-                name: classname,
-                fileEntity: file,
-                x: xPos,
-                y: yPos
+        _.each( reqData, function( reqDataItem, index ) {
+            _.each( reqDataItem, function( value, key ) {
+                serializedData.push( key + index + '=' + encodeURIComponent( value ) );
             });
-        }
+        });
+
+        $.ajax({
+            data: serializedData.join( '&' ),
+            url: 'api/create_sprite',
+            method: 'POST',
+            success: function( json ) {
+
+            }
+        });
     }
 });
 
@@ -266,7 +89,7 @@ Sprite.View.CanvasElement = Backbone.View.extend({
         this.el.style.left   = dta.x + 'px';
         this.el.style.width  = dta.w + 'px';
         this.el.style.height = dta.h + 'px';
-        this.el.style.backgroundImage = 'url(' + dta.fileURL + ')';
+        this.el.style.backgroundImage = 'url(' + dta.fileContent + ')';
         this.el.setAttribute( 'data-id', this.model.cid );
     }
 });
@@ -303,5 +126,5 @@ Sprite.View.CSSElement = Backbone.View.extend({
 });
 
 $( document ).ready( function() {
-    new Sprite.View.Document();
+    new Sprite.View.Document({ model: new Sprite.Collection.CanvasElements() });
 });
