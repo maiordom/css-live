b91299053aa707a4652682f5706b69027cbce12c
maiordom <maiordom@yandex.ru>
2016-06-05 01:13:57 +0300

    update task17, dark-hor-menu

--- dark-hor-menu/dark.js
+++ dark-hor-menu/dark.js
@@ -1,22 +1,25 @@
-$(function() {
-    var actCls = 'dark-item_active';
+(function() {
+    var activeClass = 'dark__item_active';
 
-    $( '.dark' ).each( function() {
-        setMenu( $( this ) );
+    $('.dark').each(function() {
+        setMenu($(this));
     });
 
-    function setMenu( el ) {
-        var active = el.find( '.' + actCls ), item;
-        el.on( 'click', '.dark-item', function() {
-            item = $( this );
-            if ( item.hasClass( actCls ) ) {
-                item.removeClass( actCls );
+    function setMenu(el) {
+        var active = el.find('.' + activeClass);
+
+        el.on('click', '.dark__item', function() {
+            var item = $(this);
+
+            if (item.hasClass(activeClass)) {
+                item.removeClass(activeClass);
                 active = null;
             } else {
-                active && active.removeClass( actCls );
-                active = item.addClass( actCls );
+                active && active.removeClass(activeClass);
+                active = item.addClass(activeClass);
             }
+
             return false;
         });
     }
-});
+})();
---
+++ dark-hor-menu/gulpfile.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const gulp = require('gulp');
+const connect = require('gulp-connect');
+const stylus = require('gulp-stylus');
+const autoprefixer = require('gulp-autoprefixer');
+const watch = require('gulp-watch');
+const nib = require('nib');
+const runSequence = require('run-sequence');
+const postcss = require('gulp-postcss');
+const base64 = require('postcss-base64');
+
+gulp.task('server', () => {
+    return connect.server({
+        port: 3001,
+        root: [__dirname]
+    });
+});
+
+gulp.task('stylus', () => {
+    return gulp.src('./dark.styl')
+        .pipe(stylus({
+            use: [nib()]
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', () => {
+    watch('./*.styl', () => {
+        runSequence('css');
+    });
+});
+
+gulp.task('png-to-base64', () => {
+    return gulp.src('./dark.css')
+        .pipe(postcss([
+            base64({
+                extensions: ['.png'],
+                pattern: /data:image\/(?!svg\+xml)/i,
+                prepend: 'data:image/png;'
+            })
+        ]))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('css', () => {
+	return runSequence('stylus', 'png-to-base64');
+});
+
+gulp.task('default', ['server', 'css', 'watch']);
